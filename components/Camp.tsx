58eab52f2c504f6ba3823d7bf79aa20aa2256a5e
MenathNDGD <nadungmenath1@gmail.com>
2025-03-01 20:54:17 +0530

    Add id attribute to Camp component for improved accessibility

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -17,6 +17,7 @@ const CampSite = ({
 }: CampSiteProps) => {
   return (
     <div
+      id="camp"
       className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
