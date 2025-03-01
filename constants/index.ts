340938da2e30256e973a25189b27fa97cbfb7ad9
MenathNDGD <nadungmenath1@gmail.com>
2025-03-01 20:19:00 +0530

    Refactor constants to use structured objects for features, footer links, and social media, enhancing clarity and maintainability

--- constants/index.ts
+++ constants/index.ts
@@ -20,28 +20,24 @@ export const FEATURES = [
   {
     title: "Real maps can be offline",
     icon: "/map.svg",
-    variant: "green",
     description:
       "We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location",
   },
   {
     title: "Set an adventure schedule",
     icon: "/calendar.svg",
-    variant: "green",
     description:
       "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
   },
   {
     title: "Technology using augment reality",
     icon: "/tech.svg",
-    variant: "green",
     description:
       "Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection",
   },
   {
     title: "Many new locations every month",
     icon: "/location.svg",
-    variant: "orange",
     description:
       "Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing",
   },
@@ -52,17 +48,21 @@ export const FOOTER_LINKS = [
   {
     title: "Learn More",
     links: [
-      "About Hilink",
-      "Press Releases",
-      "Environment",
-      "Jobs",
-      "Privacy Policy",
-      "Contact Us",
+      { name: "About Hilink", url: "/about-hilink" },
+      { name: "Press Releases", url: "/press-releases" },
+      { name: "Environment", url: "/environment" },
+      { name: "Jobs", url: "/jobs" },
+      { name: "Privacy Policy", url: "/privacy-policy" },
+      { name: "Contact Us", url: "/contact-us" },
     ],
   },
   {
     title: "Our Community",
-    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
+    links: [
+      { name: "Climbing Xixixi", url: "/climbing-xixixi" },
+      { name: "Hiking Hilink", url: "/hiking-hilink" },
+      { name: "Hilink Kinthill", url: "/hilink-kinthill" },
+    ],
   },
 ];
 
@@ -77,10 +77,18 @@ export const FOOTER_CONTACT_INFO = {
 export const SOCIALS = {
   title: "Social",
   links: [
-    "/facebook.svg",
-    "/instagram.svg",
-    "/twitter.svg",
-    "/youtube.svg",
-    "/wordpress.svg",
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com",
+      icon: "/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com",
+      icon: "/instagram.svg",
+    },
+    { name: "Twitter", url: "https://twitter.com", icon: "/twitter.svg" },
+    { name: "YouTube", url: "https://www.youtube.com", icon: "/youtube.svg" },
+    { name: "WordPress", url: "https://wordpress.com", icon: "/wordpress.svg" },
   ],
 };
